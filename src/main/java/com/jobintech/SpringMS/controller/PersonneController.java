4b66d11249111fbc9c001a1b05e8863a918f78ef
salah_regbi <regbisalah@gmail.com>
2025-12-24 10:26:23 +0100

    nicehh

--- src/main/java/com/jobintech/SpringMS/controller/PersonneController.java
+++ src/main/java/com/jobintech/SpringMS/controller/PersonneController.java
@@ -3,10 +3,14 @@ package com.jobintech.SpringMS.controller;
 import com.jobintech.SpringMS.model.Personne;
 import com.jobintech.SpringMS.repositories.PersonneRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -17,18 +21,29 @@ public class PersonneController {
     public List<Personne> listePersonnes() {
         return personneRepository.findAll();
     }
+
+
     @GetMapping("/Personnes/{id}")
-
-    public Personne findPersonneById( @PathVariable Long id){
-        return personneRepository.findById(id).get();
+    public ResponseEntity<Personne> findPersonneById( @PathVariable Long id){
+        Optional<Personne> personne = personneRepository.findById(id);
+        if (personne.isPresent()) {
+            return new ResponseEntity<>(personneRepository.findById(id).get(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
-
     @PostMapping("/Personnes")
-    public Personne savePersonne(@RequestBody Personne personne) {
-        return personneRepository.save(personne);
+    public ResponseEntity<Personne> savePersonne(@RequestBody Personne personne) {
+        Personne savedPersonne = personneRepository.save(personne);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/Personnes/" + savedPersonne.getId());
+        return new ResponseEntity<>(savedPersonne, headers, HttpStatus.CREATED);
     }
+
+
     @DeleteMapping("/Personnes/{id}")
     public void deletePersonne(@PathVariable Long id){
         personneRepository.deleteById(id);
     }
 }
+
